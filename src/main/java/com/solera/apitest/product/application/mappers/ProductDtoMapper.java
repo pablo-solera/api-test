b35e0634c4b284b5e5348a510661496b8562987f
Avila <pablo.avila@solera.com>
2026-01-07 09:25:40 +0100

    Workflow updated

--- src/main/java/com/solera/apitest/product/application/mappers/ProductDtoMapper.java
+++ src/main/java/com/solera/apitest/product/application/mappers/ProductDtoMapper.java
@@ -9,21 +9,36 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface ProductMapper {
+public interface ProductDtoMapper  {
+
+    Product toDomain(CreateProductRequestDto dto);
+
+    ProductResponseDto toResponse(Product product);
 
     // DTO de request → Entidad de dominio
-    @Mapping(target = "name", source = "dto.name")
+   /* @Mapping(target = "name", source = "dto.name")
     @Mapping(target = "description", source = "dto.description")
     @Mapping(target = "category", source = "category")
-    Product toDomain(CreateProductRequestDto dto, Category category);
+    Product toDomain(CreateProductRequestDto dto, Category category);*/
+
+    Product toDomain(CreateProductRequestDto dto);
+
+    ProductResponseDto toDto(Product product);
 
     // Entidad de dominio → DTO de response
-    @Mapping(target = "id", source = "product.id")
+   /* @Mapping(target = "id", source = "product.id")
     @Mapping(target = "name", source = "product.name")
     @Mapping(target = "categoryName", source = "product.category.name")
-    ProductResponseDto toDto(Product product);
+    ProductResponseDto toDto(Product product);*/
 
     ProductEntity toEntity(Product product);
 
-    Product toDomain(ProductEntity entity, Category category);
+   /* @Mapping(target = "id", source = "entity.id")
+    @Mapping(target = "name", source = "entity.name")
+    @Mapping(target = "description", source = "entity.description")
+    @Mapping(target = "category", source = "category")
+    @Mapping(target = "price", source = "entity.price")
+    Product toDomain(ProductEntity entity, Category category);*/
+
+    Product toDomain(ProductEntity entity);
 }
